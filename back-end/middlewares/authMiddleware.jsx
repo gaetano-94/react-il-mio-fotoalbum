21b67e5b2c32bfe8b5dc352e508b95933cb6b086
gaetano-94 <gaetano.panico1994@gmail.com>
2024-06-27 11:38:35 +0200

    feat: authMiddleware

---
+++ back-end/middlewares/authMiddleware.jsx
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.sendStatus(403);
+  jwt.verify(token, 'secret', (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;

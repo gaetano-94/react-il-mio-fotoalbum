b95a8aeb372ea8a27d76d26cac1d7ac38cf46c2f
gaetano-94 <gaetano.panico1994@gmail.com>
2024-06-27 15:05:37 +0200

    feat: validation for foto

--- back-end/controllers/fotoController.js
+++ back-end/controllers/fotoController.js
@@ -1,63 +1,83 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { validationResult } = require('express-validator');
 
 const getAllFoto = async (req, res) => {
   try {
     const foto = await prisma.foto.findMany();
     res.json(foto);
   } catch (error) {
-    res.status(500).json({ error: 'Errore durante il recupero delle foto' });
+    res.status(500).json({ error: 'Fetching photos failed' });
   }
 };
 
 const getFotoById = async (req, res) => {
   const { id } = req.params;
   try {
-    const foto = await prisma.foto.findUnique({ where: { id: parseInt(id) } });
-    if (!foto) return res.status(404).json({ error: 'Foto non trovata' });
+    const foto = await prisma.foto.findUnique({
+      where: { id: parseInt(id, 10) },
+    });
     res.json(foto);
   } catch (error) {
-    res.status(500).json({ error: 'Errore durante il recupero della foto' });
+    res.status(500).json({ error: 'Fetching photo failed' });
   }
 };
 
 const createFoto = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { titolo, descrizione, immagine, visibile, categorie } = req.body;
+
   try {
     const foto = await prisma.foto.create({
-      data: { titolo, descrizione, immagine, visibile, categorie },
+      data: {
+        titolo,
+        descrizione,
+        immagine,
+        visibile,
+        categorie: { connect: categorie.map((id) => ({ id })) },
+      },
     });
-    res.json(foto);
+    res.status(201).json(foto);
   } catch (error) {
-    res.status(500).json({ error: 'Errore durante la creazione della foto' });
+    res.status(500).json({ error: 'Creating photo failed' });
   }
 };
 
 const updateFoto = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { id } = req.params;
   const { titolo, descrizione, immagine, visibile, categorie } = req.body;
+
   try {
     const foto = await prisma.foto.update({
-      where: { id: parseInt(id) },
-      data: { titolo, descrizione, immagine, visibile, categorie },
+      where: { id: parseInt(id, 10) },
+      data: {
+        titolo,
+        descrizione,
+        immagine,
+        visibile,
+        categorie: { set: categorie.map((id) => ({ id })) },
+      },
     });
     res.json(foto);
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Errore durante l'aggiornamento della foto" });
+    res.status(500).json({ error: 'Updating photo failed' });
   }
 };
 
 const deleteFoto = async (req, res) => {
   const { id } = req.params;
   try {
-    await prisma.foto.delete({ where: { id: parseInt(id) } });
-    res.json({ message: 'Foto cancellata con successo' });
+    await prisma.foto.delete({ where: { id: parseInt(id, 10) } });
+    res.status(204).end();
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: 'Errore durante la cancellazione della foto' });
+    res.status(500).json({ error: 'Deleting photo failed' });
   }
 };
 

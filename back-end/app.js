5db851696001e742a732a62e86d3b9a52f405ab4
gaetano-94 <gaetano.panico1994@gmail.com>
2024-06-27 12:50:56 +0200

    feat: authRoutes and app.js and refactor for js invece di jsx

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const fotoRoutes = require('./routes/fotoRoutes');
+const categoriaRoutes = require('./routes/categoriaRoutes');
 const app = express();
 
 require('dotenv').config();
 const { PORT } = process.env;
 const port = PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/foto', fotoRoutes);
+app.use('/api/categorie', categoriaRoutes);
 
 app.listen(port, () => {
   console.log(`Server attivo su http://localhost:${port}`);

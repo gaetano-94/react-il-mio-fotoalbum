e457bbc7695d63de15e8fef0a20075f029e298ae
gaetano-94 <gaetano.panico1994@gmail.com>
2024-06-27 15:24:33 +0200

    feat: authMiddleware

--- back-end/middlewares/authMiddleware.js
+++ back-end/middlewares/authMiddleware.js
@@ -1,13 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const authenticateToken = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(403);
-  jwt.verify(token, 'secret', (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, config.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token is not valid' });
+    }
+
+    req.userId = decoded.userId;
     next();
   });
 };
 
-module.exports = authenticateToken;
+module.exports = authMiddleware;

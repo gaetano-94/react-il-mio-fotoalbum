dca71769940739d147e926e3b0b2e1e0f8694262
gaetano-94 <gaetano.panico1994@gmail.com>
2024-06-28 11:15:22 +0200

    feat: navbar

---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/admin/foto">Admin Foto</Link>
+      <Link to="/admin/categorie">Admin Categorie</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
